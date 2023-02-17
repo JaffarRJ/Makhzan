6d571ddafb740c06127ecc3b271a253695588228
kashifalidevstudio <kashifali@devstudio.us>
2023-02-17 15:23:43 +0500

    party transasctions validations added

--- app/Http/Requests/Api/AccountSubAccount/UpdateRequest.php
+++ app/Http/Requests/Api/AccountSubAccount/UpdateRequest.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Requests\Api\AccountSubAccount;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\BaseRequest;
 
-class UpdateRequest extends FormRequest
+class UpdateRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class UpdateRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class UpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'id' => 'required|exists:account_sub_accounts,id',
+            'account_id' => 'required|exists:accounts,id',
+            'sub_account_id' => 'required|exists:sub_accounts,id'
         ];
     }
 }
